296ace6c05153d2d3039753431a19db9c57c268c
Farioso Fernando <farioso.f@gmail.com>
2023-10-10 01:14:16 +0200

    features: backup and restore backup

--- lib/setup.dart
+++ lib/setup.dart
@@ -3,6 +3,60 @@ import 'package:args/args.dart';
 import 'package:hive/hive.dart';
 import 'package:path/path.dart' as p;
 
+Future<void> restoreFromBackup(String boxPath, [String? backupPath]) async {
+  final defaultBackupPath = p.join(
+      p.dirname(boxPath), "${p.basenameWithoutExtension(boxPath)}_backup.hive");
+  final restorePath = backupPath ?? defaultBackupPath;
+
+  if (!File(restorePath).existsSync()) {
+    print('\n?:.');
+    print('└─── Backup não encontrado em $restorePath.');
+    print('\n');
+    return;
+  }
+
+  try {
+    // TODO: Atualmente, a restauração do backup substitui completamente o arquivo original.
+    // Posso implementar uma abordagem de "mesclagem" em vez de substituição direta.
+
+    // nota: Isso exigiria uma lógica definida para determinar como os dados devem ser mesclados.
+
+    // Por exemplo, em caso de conflitos entre os dados do backup e os originais,
+    // preciso decidir qual conjunto de dados teria prioridade.
+
+    final originalFile = File(boxPath);
+    if (originalFile.existsSync()) {
+      originalFile.deleteSync();
+    }
+
+    File(restorePath).copySync(boxPath);
+    print('\n?:.');
+    print('└─── Backup restaurado com sucesso de $restorePath para $boxPath.');
+  } catch (e) {
+    print('\n?:.');
+    print('└─── Erro ao restaurar o backup: $e');
+    print('\n');
+  }
+}
+
+Future<void> backupBox(String boxPath, [String? destDir]) async {
+  final backupLocation = destDir ?? p.dirname(boxPath);
+  final backupFileName = "${p.basenameWithoutExtension(boxPath)}_backup.hive";
+  final backupFilePath = p.join(backupLocation, backupFileName);
+
+  final boxFile = File(boxPath);
+  if (await boxFile.exists()) {
+    await boxFile.copy(backupFilePath);
+    print('\n?:.');
+    print('└─── Backup criado em: $backupFilePath');
+    print('\n');
+  } else {
+    print('\n?:.');
+    print('└─── Erro: Não foi possível encontrar a caixa.');
+    print('\n');
+  }
+}
+
 Future<void> deleteData(String boxPath, String key) async {
   final box = await Hive.openBox(p.basename(boxPath).split('.').first);
 
@@ -107,6 +161,14 @@ void setup(List<String> args) async {
   deleteCommand.addOption('key',
       abbr: 'k', help: 'Chave do dado a ser removido.');
 
+  var backupCommand = cmdParser.addCommand('backup');
+  backupCommand.addOption('destination',
+      abbr: 'd', help: 'Localização onde o backup será salvo.');
+
+  var restoreCommand = cmdParser.addCommand('restore');
+  restoreCommand.addOption('source',
+      abbr: 's', help: 'Localização do arquivo de backup.');
+
   String path = results['path'] as String;
 
   Hive.init(p.dirname(path));
@@ -150,6 +212,12 @@ void setup(List<String> args) async {
       } else {
         print('Por favor, forneça uma chave para remover o dado.');
       }
+    } else if (cmdResults.command?.name == 'backup') {
+      var destDir = cmdResults.command?['destination'];
+      await backupBox(results['path'] as String, destDir);
+    } else if (cmdResults.command?.name == 'restore') {
+      var backupLocation = cmdResults.command?['source'];
+      await restoreFromBackup(results['path'] as String, backupLocation);
     } else {
       print('Comando desconhecido.');
     }
